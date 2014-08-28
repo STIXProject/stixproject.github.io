cf60e43430ced195aed6a9d5c090d5192ee31e3d
Ben <x@d-str.cc>
2014-08-28 20:08:49 +0000

    whoops forgot consumer script

--- documentation/idioms/incident-timestamps/incident-timestamps_consumer.py
+++ documentation/idioms/incident-timestamps/incident-timestamps_consumer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Copyright (c) 2014, The MITRE Corporation. All rights reserved.
+# See LICENSE.txt for complete terms.
 
 import sys
 from stix.core import STIXPackage, STIXHeader
