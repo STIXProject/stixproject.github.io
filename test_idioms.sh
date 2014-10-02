6d3fa195e7e908e5a8e568f680268519faa4734a
Greg Back <gback@mitre.org>
2014-10-02 07:48:16 -0500

    Clean up output of test_idioms.sh script

--- test_idioms.sh
+++ test_idioms.sh
@@ -3,6 +3,9 @@
 
 validator=$PWD/stix-validator/sdv.py
 
+function realpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
+
+RETVAL=0
 for dir in ./documentation/idioms/*; do
     if [ -d $dir ] 
     then
@@ -11,14 +14,14 @@ for dir in ./documentation/idioms/*; do
         for producer in ./*producer.py ; do
         if [ -e $producer ]
         then
-            echo Running $producer
             `python $producer > out.xml 2>/dev/null`
             if [[ -e out.xml && $? -eq 0 ]]
             then
-                echo "."
+                echo -n "."
             else
-                echo "error in $producer"
-                exit 1
+                echo -e "\nERROR running $(realpath $producer)"
+                RETVAL=1
+                continue
             fi
         fi
         done
@@ -26,30 +29,30 @@ for dir in ./documentation/idioms/*; do
         for consumer in ./*consumer.py ; do
         if [ -e $consumer ]
         then
-            echo Running $consumer
             `python $consumer out.xml > out.parsed 2>/dev/null`
             if [[ -e out.parsed && $? -eq 0 ]]
             then
-                echo "."
+                echo -n "."
             else
-                echo "error in $consumer"
-                exit 1
+                echo -e "\nERROR running $(realpath $consumer)"
+                RETVAL=1
+                continue
             fi
         fi
         done
-        
+
         for xmlfile in ./*xml ; do
         if [ -e $xmlfile ]
         then
-            echo Checking $xmlfile for well-formedness
             $($validator $xmlfile | grep -q INVALID)
-            if [[ $? -eq 0 ]]
+            if [ $? -eq 0 ]
             # the file had validation errors
             then
-                echo "validation failure for $xmlfile"
-                exit 1
+                echo -e "\nERROR: $(realpath $xmlfile) had validation errors"
+                RETVAL=1
+                continue
             else
-                echo "."
+                echo -n "."
             fi
         fi
         done
@@ -58,4 +61,5 @@ for dir in ./documentation/idioms/*; do
     fi
 done;
 
-exit 0
+echo -e "\nDone!"
+exit $RETVAL
