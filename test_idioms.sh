a41263f4263a06dc481e7043c2bcc34b6c5ea9f0
Michael Chisholm <chisholm@mitre.org>
2015-11-24 21:05:36 -0500

    Revamped test_idioms script with a saner design.

--- test_idioms.sh
+++ test_idioms.sh
@@ -1,66 +1,110 @@
 #!/bin/bash
-#iterate directories to find ones with .py files
+
+: ${VERBOSE:=0}
 
 validator=stix-validator.py
 
 function realpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
 
-RETVAL=0
-for dir in ./documentation/idioms/*; do
-    if [ -d $dir ] 
-    then
-        pushd $dir > /dev/null  #change to idiom directory
+function log
+{
+    [ "$VERBOSE" -ne 0 ] && echo -e "$1"
+}
 
-        for producer in ./*producer.py ; do
-        if [ -e $producer ]
-        then
-            `python $producer > out.xml 2>/dev/null`
-            if [[ -e out.xml && $? -eq 0 ]]
-            then
-                echo -n "."
-            else
-                echo -e "\nERROR running $(realpath $producer)"
-                RETVAL=1
-                continue
-            fi
-        fi
-        done
+function run_producer_consumer
+{
+    local producer=$1
+    local stem="${producer%producer.py}"
+    local consumer="${stem}consumer.py"
+    local outfile="${stem}out.xml"
+    local parsedfile="${stem}parsed.txt"
 
-        for consumer in ./*consumer.py ; do
-        if [ -e $consumer ]
-        then
-            `python $consumer out.xml > out.parsed 2>/dev/null`
-            if [[ -e out.parsed && $? -eq 0 ]]
-            then
-                echo -n "."
-            else
-                echo -e "\nERROR running $(realpath $consumer)"
-                RETVAL=1
-                continue
-            fi
-        fi
-        done
-
-        for xmlfile in ./*xml ; do
-        if [ -e $xmlfile ]
-        then
-            $($validator $xmlfile > /dev/null)
-            if [ $? -ne 0 ]
-            # the file had validation errors
-            then
-                echo -e "\nERROR: $(realpath $xmlfile) had validation errors"
-                
-                RETVAL=1
-                continue
-            else
-                echo -n "."
-            fi
-        fi
-        done
-
-        popd > /dev/null #return to root dir
+    rm -f "$outfile" "$parsedfile"
+    
+    log "  Producer: $producer"
+    python "$producer" > "$outfile" 2>/dev/null
+    if [[ $? -ne 0 || ! -e "$outfile" ]] ; then
+        return 1
     fi
-done;
+
+    if [ -f "$consumer" ] ; then
+	log "  Consumer: $consumer"
+        python "$consumer" "$outfile" > "$parsedfile" 2>/dev/null
+        if [[ $? -ne 0 || ! -e "$parsedfile" ]] ; then
+            return 1
+        fi
+    fi
+
+    return 0
+}
+
+# User can specify their own idiom dirs if they want.
+if [ $# -gt 0 ] ; then
+    IDIOM_DIRS="$@"
+else
+    IDIOM_DIRS=./documentation/idioms/*
+fi
+
+# Not every idiom dir has any *.py files.
+# Let fileglobs expand to "" in that case.
+shopt -s nullglob
+
+RETVAL=0
+for dir in $IDIOM_DIRS; do
+    [ -d $dir ] || continue
+
+    log "Idiom: $dir"
+    pushd $dir > /dev/null  #change to idiom directory
+
+    # Assume sets of paired scripts, named *producer.py and
+    # *consumer.py.  The output of *producer.py is checked
+    # with *consumer.py.
+    #
+    # Any misc scripts which don't conform to this naming
+    # convention are just run and checked for exit status.
+    #
+    # Dangling producer scripts are simply run for exit status.
+    # Dangling consumer scripts are ignored.
+
+    for scriptfile in *.py ; do
+	case "$scriptfile" in
+	    *producer.py)
+		run_producer_consumer "$scriptfile"
+		;;
+	    *consumer.py)
+		true
+		;;
+	    *)
+		log "  Run: $scriptfile"
+		python "$scriptfile" > /dev/null 2>&1
+		;;
+        esac
+
+	if [ $? -eq 0 ] ; then
+	    # maybe in verbose mode, the "."s would be drowned out
+	    # by log messages and be kinda pointless...
+            [ "$VERBOSE" -eq 0 ] && echo -n "."
+	else
+            echo -e "\nERROR running $(realpath $scriptfile)"
+	    RETVAL=1
+	fi
+    done
+
+    for xmlfile in ./*.xml ; do
+	log "  Validate: $xmlfile"
+        python "$validator" "$xmlfile" > /dev/null 2>&1
+        if [ $? -ne 0 ]
+        # the file had validation errors
+        then
+            echo -e "\nERROR: $(realpath $xmlfile) had validation errors"
+            RETVAL=1
+        else
+            [ "$VERBOSE" -eq 0 ] && echo -n "."
+        fi
+    done
+
+    popd > /dev/null #return to root dir
+done
 
 echo -e "\nDone!"
 exit $RETVAL
